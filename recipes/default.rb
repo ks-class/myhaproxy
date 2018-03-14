d125ccb9b735fea44652032c732fbf22dae6e0f7
ks-class <Kirk.Shipley@mmc.com>
2018-03-14 14:34:12 +0000

    Version 0.2.0 added apache_web lb member

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,6 +9,11 @@ node.default['haproxy']['members'] = [{
   'ipaddress' => '34.207.164.188',
   'port' => 80,
   'ssl_port' => 80
+  },{
+  'hostname' => 'ec2-34-229-107-1.compute-1.amazonaws.com',
+  'ipaddress' => '34.229.107.1',
+  'port' => 80,
+  'ssl_port' => 80 
   }]
 
   include_recipe 'haproxy::manual'
