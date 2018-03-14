c40eed009bda15d048e57f2e65122210f4870ea3
ks-class <Kirk.Shipley@mmc.com>
2018-03-14 18:16:54 +0000

    Version 1.0.0 dynamically updates the load balancing pool based on role

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,16 +4,20 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-node.default['haproxy']['members'] = [{
-  'hostname' => 'ec2-34-207-164-188.compute-1.amazonaws.com',
-  'ipaddress' => '34.207.164.188',
-  'port' => 80,
-  'ssl_port' => 80
-  },{
-  'hostname' => 'ec2-34-229-107-1.compute-1.amazonaws.com',
-  'ipaddress' => '34.229.107.1',
-  'port' => 80,
-  'ssl_port' => 80 
-  }]
+all_web_nodes = search('node','role:web_server')
 
-  include_recipe 'haproxy::manual'
+members = []
+
+all_web_nodes.each do |web_node|
+  member = {
+    'hostname' => web_node['cloud']['public_hostname'],
+    'ipaddress' => web_node['cloud']['public_ipv4'],
+    'port' => 80,
+    'ssl_port' =>80
+    }
+    members.push(member)
+end
+
+node.default['haproxy']['members'] = members
+
+include_recipe 'haproxy::manual'
