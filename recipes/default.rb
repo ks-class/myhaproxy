8516f7bd54080a4da99fe51d4cfff50d92dccd41
ks-class <Kirk.Shipley@mmc.com>
2018-03-14 13:55:49 +0000

    Initial Commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+node.default['haproxy']['members'] = [{
+  'hostname' => 'ec2-34-207-164-188.compute-1.amazonaws.com',
+  'ipaddress' => '34.207.164.188',
+  'port' => 80,
+  'ssl_port' => 80
+  }]
+
+  include_recipe 'haproxy::manual'
